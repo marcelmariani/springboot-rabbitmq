7880ccf8a2cdde3de5b6d2f7c20e6288b7478c6f
marcelmariani <marcel.vazmariani@gmail.com>
2024-08-15 17:30:56 -0300

    Java Spring boot with RabbitMq First commit

---
+++ springboot-rabbitmq/src/main/java/com/marcelmariani/entity/CustomerProducer.java
@@ -0,0 +1,35 @@
+package com.marcelmariani.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerProducer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "documentId")
+	@JsonProperty("document_id")
+	private String documentId;
+}
